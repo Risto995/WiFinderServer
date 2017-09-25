908092d49ab8e59ffa19ba76332df59dfc3a0bfc
Risto Kekovic <risto.kekovic@gmail.com>
2017-09-25 15:04:42 +0200

    Set up Heroku credentials

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'eu-cdbr-west-01.cleardb.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_2334895b9683c26'),
+            'username' => env('DB_USERNAME', 'b3351013d227e4'),
+            'password' => env('DB_PASSWORD', '5c70392d'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
